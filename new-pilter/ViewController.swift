49cfef67133952a78bbb615ea1614ec8c2f8bd8c
new0628 <new970628@naver.com>
2025-07-01 19:03:15 +0900

    Initial Commit

---
+++ new-pilter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  new-pilter
+//
+//  Created by 이상원 on 7/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ new-pilterTests/new_pilterTests.swift
@@ -0,0 +1,17 @@
+//
+//  new_pilterTests.swift
+//  new-pilterTests
+//
+//  Created by 이상원 on 7/1/25.
+//
+
+import Testing
+@testable import new_pilter
+
+struct new_pilterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
